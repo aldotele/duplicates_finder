2724f684a1ee7d169742c9be67d01ab368205ea6
aldotele <aldo.telese@hotmail.it>
2020-11-23 08:19:07 +0100

    condition added to print output

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ rd = input('enter a root directory (leave empty for current directory): ')
 # note: file extensions have to be written starting from the DOT: e.g. ".py", ".png", ".txt" etc.
 while True:
     file_extension = input('enter a file extension (leave empty for all extensions): ')
-    if file_extension[0] == '.':
+    if file_extension == '':
+        break
+    elif file_extension[0] == '.':
         break
     else:
         print('NOT VALID. Your file extension has to start with a DOT (".txt", ".png", etc.): ')
@@ -17,10 +19,11 @@ while True:
 duplicate_pairs = find_duplicates(rd, file_extension)
 
 print()
-print('Detailed results of the analysis: ')
-# showing a sequence of duplicates-original pairs
-for duplicate, original in duplicate_pairs:
-    print(duplicate, '__ DUPLICATE OF __', original)
+if duplicate_pairs != []:
+    print('Detailed results of the analysis: ')
+    # showing a sequence of duplicates-original pairs
+    for duplicate, original in duplicate_pairs:
+        print(duplicate, '__ DUPLICATE OF __', original)
 
 # Storing only the duplicates for later use (by extracting the first element of each tuple)
 # the original files will not be in the duplicates list
@@ -33,6 +36,7 @@ print()
 
 print('In total, {} DUPLICATES were found'.format(len(duplicates)))
 # showing only the sequence of duplicates
-print('here is the list of DUPLICATES that were found:')
-for file in duplicates:
-    print(file)
+if duplicates != []:
+    print('here is the list of DUPLICATES that were found:')
+    for file in duplicates:
+        print(file)
