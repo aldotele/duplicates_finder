93967c1e9acae246048c325247b1a9ac4b1eb736
aldotele <aldo.telese@hotmail.it>
2021-08-01 19:05:19 +0200

    rename dupli as original plus DUP

--- dupli_lib/dupli_finder.py
+++ dupli_lib/dupli_finder.py
@@ -21,7 +21,9 @@ def find_duplicates(rootdir='', filext=''):
         for file in files:
             # if no extension is provided, all encountered files will be pushed into the list
             if not filext:
-                all_files.append(os.path.join(root, file))
+                extract_ext = os.path.splitext(file)
+                if extract_ext[1]:  # only files with actual extensions will be parsed
+                    all_files.append(os.path.join(root, file))
             else:
                 # otherwise, the first thing is to calculate how many characters is the extension made of
                 fromindex = len(filext)
@@ -33,7 +35,8 @@ def find_duplicates(rootdir='', filext=''):
     # a list will store a series of tuples with a pair of duplicates
     d_md5 = {}
     duplipairs = []
-    
+    idx_dup = 0
+
     for file in all_files:
         hashing = md5checksum(file)
         # if the md5 hashing is not in the dictionary, it will be added as a key, with its file as a value
@@ -41,8 +44,12 @@ def find_duplicates(rootdir='', filext=''):
             d_md5[hashing] = file
         # if the md5 hashing is already a key, it means the file is a duplicate
         # in this case a tuple (duplicate, original) will be added to the duplipairs list
-        # the original file is the file stored as a value of the md5 hashing key 
+        # the original file is the file stored as a value of the md5 hashing key
         else:
+            idx_dup += 1
+            # renaming the duplicate file as the original file plus _DUP
+            name, ext = os.path.splitext(d_md5[hashing])  # this returns
+            os.rename(file, name + "_DUP_" + str(idx_dup) + ext)
             duplipairs.append((file, d_md5[hashing]))
 
     # the output will be the list of (duplicate, original) pairs
